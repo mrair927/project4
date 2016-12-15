c116224e15f8882925f84ed24961136e53351afb
Meghan.rair <Meghan.rair@gmail.com>
2016-12-15 13:53:50 -0500

    bug fix

--- routes/web.php
+++ routes/web.php
@@ -17,23 +17,23 @@ Route::get('/show-login-status', function() {
 * Task resources
 */
 # Index page to show all the tasks
-Route::get('/tasks', 'taskController@index')->name('tasks.index')->middleware('auth');
+Route::get('/tasks', 'TaskController@index')->name('tasks.index')->middleware('auth');
 # Similar to Index page to show all the completed tasks
-Route::get('/tasks/completed', 'taskController@completed')->name('tasks.completed')->middleware('auth');
+Route::get('/tasks/completed', 'TaskController@completed')->name('tasks.completed')->middleware('auth');
 # Show a form to create a new task
-Route::get('/tasks/create', 'taskController@create')->name('tasks.create')->middleware('auth');
+Route::get('/tasks/create', 'TaskController@create')->name('tasks.create')->middleware('auth');
 # Process the form to create a new task
-Route::post('/tasks', 'taskController@store')->name('tasks.store');
+Route::post('/tasks', 'TaskController@store')->name('tasks.store');
 # Show an individual task
-Route::get('/tasks/{title}', 'taskController@show')->name('tasks.show');
+Route::get('/tasks/{title}', 'TaskController@show')->name('tasks.show');
 # Show form to edit a task
-Route::get('/tasks/{id}/edit', 'taskController@edit')->name('tasks.edit');
+Route::get('/tasks/{id}/edit', 'TaskController@edit')->name('tasks.edit');
 # Process form to edit a task
-Route::put('/tasks/{id}', 'taskController@update')->name('tasks.update');
+Route::put('/tasks/{id}', 'TaskController@update')->name('tasks.update');
 # Get route to confirm deletion of task
-Route::get('/tasks/{id}/delete', 'taskController@delete')->name('tasks.destroy');
+Route::get('/tasks/{id}/delete', 'TaskController@delete')->name('tasks.destroy');
 # Delete route to actually destroy the task
-Route::delete('/tasks/{id}', 'taskController@destroy')->name('tasks.index');
+Route::delete('/tasks/{id}', 'TaskController@destroy')->name('tasks.index');
 
 Auth::routes();
 
